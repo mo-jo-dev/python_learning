b20632faa17faf8822fda95cd04c799a2092e14d
Mohit Joshi <b210055@nitsikkim.ac.in>
2024-01-30 16:10:47 +0530

    conditions program

--- 30_01_2024 (LAB 1)/Conditions.py
+++ 30_01_2024 (LAB 1)/Conditions.py
@@ -1,7 +1,15 @@
 # condition
+print("Enter your Gender: ")
+gender = input()
 print("Enter your current age: ")
-current_age = int(input())
-if age > 21:
-    print("Hooray!! You can marry")
-else:
-    print("Sorry!! You can't marry")
+age = int(input())
+if gender == "MALE" or gender == "Male" or gender == "male":
+    if age > 21:
+        print("Hooray!! You can marry")
+    else:
+        print("Sorry!! You can't marry")
+else:    
+    if age > 18:
+        print("Hooray!! You can marry")
+    else:
+        print("Sorry!! You can't marry")
